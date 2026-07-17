7cce759f1bacfdb779df34172b9b305ebbb6b64b
NicolasUrdiales <nicolas.urdiales@estudiantes.unahur.edu.ar>
2026-07-17 11:59:41 -0300

    se comento un script con errores

--- Assets/_Project/Scripts/EngineController.cs
+++ Assets/_Project/Scripts/EngineController.cs
@@ -1,124 +1,124 @@
-using System;
-using UnityEngine;
+// using System;
+// using UnityEngine;
 
 
-namespace Excavator.Engine
-{
-    public class EngineController: MonoBehaviour
-    {
-        [SerializeField] EngineConfig config;
+// namespace Excavator.Engine
+// {
+//     public class EngineController: MonoBehaviour
+//     {
+//         [SerializeField] EngineConfig config;
 
-        [Tooltip("Debe implementar IIgnitionInput")]
-        [SerializeField] MonoBehaviour ignitionInputSource;
-
-
-
-        [Tooltip("Debe implementar IThrottleInput")]
-        [SerializeField] MonoBehaviour throttleInputSource;
-
-        [Tooltip("Opcional. Debe implementar IEngineLoadSource. Si queda vacio se usa NullLoadSource")]
-        [SerializeField] MonoBehaviour loadInputSource;
-
-        IIgnitionInput ignition;
-        IThrottleInput throttle;
-        IEngineLoadSource load;
-
-        IEngineState currentState;
-
-
-        public EngineConfig Config => config;
-        public EngineSimulationCore Simulation {get; private set;}
-        public float CurrentRpm => Simulation.CurrentRpm;
-
-        public EngineState State => currentState?.State ?? EngineState.Off;
-
-
-        public event Action<float> RPMChanged;
-        public event Action<EngineStateId> StateChanged;
-
-        public event Action StarterBegan;
-
-        public event Action EngineStarted;
+//         [Tooltip("Debe implementar IIgnitionInput")]
+//         [SerializeField] MonoBehaviour ignitionInputSource;
 
 
 
-        void Awake()
-        {
-            ignition = ignitionInputSource as IIgnitionInput;
-            throttle = throttleInputSurce as IThrottleInput;
-            load = loadInputSource as IEngineLoadSource ?? new NullLoadSource();
+//         [Tooltip("Debe implementar IThrottleInput")]
+//         [SerializeField] MonoBehaviour throttleInputSource;
+
+//         [Tooltip("Opcional. Debe implementar IEngineLoadSource. Si queda vacio se usa NullLoadSource")]
+//         [SerializeField] MonoBehaviour loadInputSource;
+
+//         IIgnitionInput ignition;
+//         IThrottleInput throttle;
+//         IEngineLoadSource load;
+
+//         IEngineState currentState;
 
 
-            if (ignition == null) Debug.LogError($"{nameof(ignitionInputSource)}  debe implementar IIgnitionInput", this);
+//         public EngineConfig Config => config;
+//         public EngineSimulationCore Simulation {get; private set;}
+//         public float CurrentRpm => Simulation.CurrentRpm;
 
-            if(throttle == null) Debug.LogError($"{nameof(throttleInputSource)} debe implementar IThrottleInput", this);
-
-            Simulation = new EngineSimulationCore(
-                config.idleRpm,
-                config.maxRpm,
-                config.baseResponseRate,
-                normalizedRpm => config.torqueCurve.Evaluate(normalizedRpm)
-            );
-
-        }
+//         public EngineState State => currentState?.State ?? EngineState.Off;
 
 
-        void OnEnable()
-        {
-            ignition.ContactToggled += HandleContactToggled;
-            ignition.StarterEngaged += HandleStartedEngaged;
-            ChangeState(new OffState());
-        }
+//         public event Action<float> RPMChanged;
+//         public event Action<EngineStateId> StateChanged;
+
+//         public event Action StarterBegan;
+
+//         public event Action EngineStarted;
 
 
-        void OnDisable()
-        {
-            ignition.ContactToggled -= HandleContactToggled;
-            ignition.StartedEngaged -= HandleStarterEngaged;
-        }
-        void Update() => ignition.Tick();
 
-        void FixedUpdate()
-        {
-            currentState.Tick(this, Time.fixedDeltaTime);
-            RPMCharged?.Invoke(CurrentRPM);
-        }
+//         void Awake()
+//         {
+//             ignition = ignitionInputSource as IIgnitionInput;
+//             throttle = throttleInputSurce as IThrottleInput;
+//             load = loadInputSource as IEngineLoadSource ?? new NullLoadSource();
 
-          void HandleContactToggled()
-        {
-            switch (CurrentStateId)
-            {
-                case EngineStateId.Off:
-                    ChangeState(new ContactOnState());
-                    break;
-                case EngineStateId.ContactOn:
-                case EngineStateId.Running:
-                    ChangeState(new OffState());
-                    break;
-            }
-        }
+
+//             if (ignition == null) Debug.LogError($"{nameof(ignitionInputSource)}  debe implementar IIgnitionInput", this);
+
+//             if(throttle == null) Debug.LogError($"{nameof(throttleInputSource)} debe implementar IThrottleInput", this);
+
+//             Simulation = new EngineSimulationCore(
+//                 config.idleRpm,
+//                 config.maxRpm,
+//                 config.baseResponseRate,
+//                 normalizedRpm => config.torqueCurve.Evaluate(normalizedRpm)
+//             );
+
+//         }
+
+
+//         void OnEnable()
+//         {
+//             ignition.ContactToggled += HandleContactToggled;
+//             ignition.StarterEngaged += HandleStartedEngaged;
+//             ChangeState(new OffState());
+//         }
+
+
+//         void OnDisable()
+//         {
+//             ignition.ContactToggled -= HandleContactToggled;
+//             ignition.StartedEngaged -= HandleStarterEngaged;
+//         }
+//         void Update() => ignition.Tick();
+
+//         void FixedUpdate()
+//         {
+//             currentState.Tick(this, Time.fixedDeltaTime);
+//             RPMCharged?.Invoke(CurrentRPM);
+//         }
+
+//           void HandleContactToggled()
+//         {
+//             switch (CurrentStateId)
+//             {
+//                 case EngineStateId.Off:
+//                     ChangeState(new ContactOnState());
+//                     break;
+//                 case EngineStateId.ContactOn:
+//                 case EngineStateId.Running:
+//                     ChangeState(new OffState());
+//                     break;
+//             }
+//         }
  
-        void HandleStarterEngaged()
-        {
-            if (CurrentStateId == EngineStateId.ContactOn)
-                ChangeState(new StartingState());
-        }
+//         void HandleStarterEngaged()
+//         {
+//             if (CurrentStateId == EngineStateId.ContactOn)
+//                 ChangeState(new StartingState());
+//         }
  
-        public void ChangeState(IEngineState next)
-        {
-            currentState?.Exit(this);
-            currentState = next;
-            StateChanged?.Invoke(currentState.Id);
-            currentState.Enter(this);
-        }
+//         public void ChangeState(IEngineState next)
+//         {
+//             currentState?.Exit(this);
+//             currentState = next;
+//             StateChanged?.Invoke(currentState.Id);
+//             currentState.Enter(this);
+//         }
  
-        public float ReadThrottle() =>
-            CurrentStateId == EngineStateId.Running ? throttle.ReadThrottle() : 0f;
+//         public float ReadThrottle() =>
+//             CurrentStateId == EngineStateId.Running ? throttle.ReadThrottle() : 0f;
  
-        public float ReadLoad() => load.ReadLoad01();
+//         public float ReadLoad() => load.ReadLoad01();
  
-        public void RaiseStarterBegan() => StarterBegan?.Invoke();
-        public void RaiseEngineStarted() => EngineStarted?.Invoke();
-    }
+//         public void RaiseStarterBegan() => StarterBegan?.Invoke();
+//         public void RaiseEngineStarted() => EngineStarted?.Invoke();
+//     }
 
-}
+// }
